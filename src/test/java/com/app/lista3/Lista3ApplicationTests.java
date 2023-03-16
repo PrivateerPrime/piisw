76f774ff3161fe71c961171ac75138cbc80abab4
Kacper Lipowicz <kacperlipowicz@gmail.com>
2023-03-16 17:24:39 +0100

    Lista 3 - init

---
+++ src/test/java/com/app/lista3/Lista3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.lista3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lista3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
