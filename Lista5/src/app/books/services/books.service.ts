6ecd6c5e28a80cbd2ed2d5d2a1c8d5cfd7480cd7
Kacper Lipowicz <kacperlipowicz@gmail.com>
2023-04-05 14:25:14 +0200

    Lista5 init

---
+++ Lista5/src/app/books/services/books.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Book } from '../model/book';
+
+const booksApiPrefix = '/api/books';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BooksService {
+
+  constructor(private readonly http: HttpClient) { }
+
+  getAllBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(booksApiPrefix);
+  }
+
+  getBookById(id: number): Observable<Book> {
+    return this.http.get<Book>(`${booksApiPrefix}/${id}`)
+  }
+}
